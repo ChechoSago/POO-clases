5af9959aa314b76e9a29d5e336954adbd4a91af3
Checho Garcia <sergiogarcia1036@gmail.com>
2022-09-11 14:42:50 -0500

    Tenemos varias dudas

--- src/com/checho/calculadora/app/AppCalculadora.java
+++ src/com/checho/calculadora/app/AppCalculadora.java
@@ -3,10 +3,16 @@ package com.checho.calculadora.app;
 import com.checho.calculadora.domino.Calculadora;
 
 public class AppCalculadora {
-    public void main(String[] args) {
-        Calculadora calculadora1 = new Calculadora("casio");
-        System.out.println(calculadora1.sumar(2,3));
+    public static void main(String[] args) {
+
+        System.out.println(Calculadora.sumar(2.3f,8));
+        System.out.println(Calculadora.sumar(2,3,8));
+        System.out.println(Calculadora.restar(2.3f,3));
+        System.out.println(Calculadora.restar(2,3,8));
+        System.out.println(Calculadora.multi(2,3));
+        System.out.println(Calculadora.multi(3,2));
+        System.out.println(Calculadora.divi(8,0));
+        System.out.println(Calculadora.divi(2,3,2));
+
     }
-
-
 }
