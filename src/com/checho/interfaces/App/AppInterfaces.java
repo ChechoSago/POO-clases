cebac4ea6b4a4037ba7a52310e2d89131503505c
Checho Garcia <sergiogarcia1036@gmail.com>
2022-10-31 18:21:48 -0500

    la tarea

--- src/com/checho/interfaces/App/AppInterfaces.java
+++ src/com/checho/interfaces/App/AppInterfaces.java
@@ -1,10 +1,7 @@
 package com.checho.interfaces.App;
 
-import com.checho.interfaces.Dominio.CelularBasico;
-import com.checho.interfaces.Dominio.CelularIntermedio;
+import com.checho.interfaces.Dominio.*;
 import com.checho.interfaces.Dominio.CelularSemiavanzado;
-import com.checho.interfaces.Dominio.CelularSemiavanzado;
-import com.checho.interfaces.Dominio.Persona;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,12 +13,14 @@ public class AppInterfaces {
         Persona alejando = new Persona("Alejando", "ASDF", "has12", 9999,"hola1");
         Persona sofia = new Persona("Sofia", "ZXCV", "has13", 0000,"hola2");
         Persona daniel = new Persona("Daniel", "QWER", "has14", 8888,"hola3");
+        Persona sergio = new Persona("Sergio", "mnbv", "has1", 7777,"hola");
 
         CelularBasico nokia = new CelularBasico("Nokia 1100", daniel);
         CelularSemiavanzado xiaomi = new CelularSemiavanzado("Xiaomi", alejando);
         CelularIntermedio huawei = new CelularIntermedio("huawei P44", sofia);
+        CelularAvanzado iPhone = new CelularAvanzado("iPhon 13 ", sergio);
 
-        tratarDeDesbloquear(asList(nokia, xiaomi, huawei), sofia);
+        tratarDeDesbloquear(asList(nokia, xiaomi, huawei, iPhone), sofia);
     }
 
     public static void tratarDeDesbloquear(List<Desbloqueable> celulares, Persona persona) {
--- src/com/checho/interfaces/Dominio/CelularAvanzado.java
+++ src/com/checho/interfaces/Dominio/CelularAvanzado.java
@@ -1,4 +1,18 @@
 package com.checho.interfaces.Dominio;
 
-public class CelularAvanzado {
+public class CelularAvanzado extends CelularSemiavanzado implements Desbloqueable {
+    private String rostroCorrecto;
+
+
+    public CelularAvanzado(String marca, Persona propietario) {
+        super(marca, propietario);
+        this.rostroCorrecto = propietario.getRostro();
+    }
+
+
+    @Override
+    public boolean desbloquear(Persona persona) {
+        return rostroCorrecto.equals(persona.getRostro())  ||  super.desbloquear(persona);
+    }
 }
+
